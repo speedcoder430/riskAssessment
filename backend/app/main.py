730d237b2b82004dc07ed5c2ec3be9b142f70a78
joelgeense <webguru318@gmail.com>
2025-03-12 08:35:44 +0100

    enhance cors configuration for better error handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.auth.signIn import router as signin_router
 from app.api.auth.signUp import router as signup_router
 from app.api.auth.refresh import router as refresh_token_router
@@ -11,6 +12,22 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
+
+# Define the origins that are allowed to communicate with your API
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    # Add other origins as needed
+]
+
+# Add CORSMiddleware to the application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(signin_router)
 app.include_router(signup_router)
